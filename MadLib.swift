e87e9ea603d8bab0edbb29f8c046ebab79323e38
Sia Ryu <sryu5320@stu.d214.org>
2024-04-15 13:34:46 -0500

    created MadLib

---
+++ MadLib.swift
@@ -0,0 +1,17 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Sia Ryu on 4/15/24.
+//
+
+import SwiftUI
+
+struct MadLib: View {
+    var body: some View {
+        NounView()
+        VerbView()
+        AdjectiveView()
+       
+    }
+}
