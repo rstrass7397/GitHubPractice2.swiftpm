a29cc08a8a00184b1a1d8ed9bbb3cfd90e2d6f8d
Ryan Strass <rstrass7397@stu.d214.org>
2024-04-15 13:22:53 -0500

    fixed an error

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            NavigationLink(destination: NounView()){ Text("Go to NounView") }
+            NavigationLink(destination: NounView()) {
+                Text("Go to NounView")
+            }
         }
     }
 }
