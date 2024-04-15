e470d33be5ebd4964f51ac161a163e8ba3baed88
Sia Ryu <sryu5320@stu.d214.org>
2024-04-15 13:26:51 -0500

    Created AdjectiveView.

---
+++ AdjectiveView.swift
@@ -0,0 +1,16 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by Sia Ryu on 4/15/24.
+//
+
+import SwiftUI
+
+struct AdjectiveView: View {
+    var body: some View {
+        NavigationLink{
+            SummaryView()
+        }
+    }
+}
